e6557f15b6b1bf3c3052de1c8dcb1bd6bba3c86e
Daniel Corcoran <danielc.92@outlook.com>
2019-08-01 10:35:24 +1000

    Added redux to app with provider and placeholder reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+
+// ant design style sheet
 import 'antd/dist/antd.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+// redux
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = () => {
+    return {
+        testState: ['test-state']
+    }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunk)
+    ));
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'))
