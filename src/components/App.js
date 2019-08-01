a08de2d2fd7b013253b72b20e91463b9e87860c4
Daniel Corcoran <danielc.92@outlook.com>
2019-08-01 11:33:42 +1000

    Created main component to nest routes inside

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import { Layout, Menu } from 'antd';
 const { Content, Header,  Footer } = Layout;
 
+
 export default class App extends Component {
 
     render() {
         return (
                <Layout>
                 <ReactHeader/>
-                <Content style={{ padding: '50px', marginTop: 64 }}>
-                <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
-                </Content>
+                <ReactMain/>
                 <ReactFooter/>
             </Layout>
         )
---
+++ src/components/ReactMain.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Layout from 'antd'
+
+
+export default class ReactMain extends Component {
+    render() {
+        return (
+            <Layout.Content style={{ padding: '50px', marginTop: 64 }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
+                
+            </div>
+            </Layout.Content>
+        )
+    }
+}
