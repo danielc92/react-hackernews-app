8c5599025b3b48448f43e7903a85a8f46ea93c69
Daniel Corcoran <danielc.92@outlook.com>
2019-08-01 20:12:36 +1000

    Modified story description, corrected DT calculation

--- src/components/stories/details/ReactStoryDescription.js
+++ src/components/stories/details/ReactStoryDescription.js
@@ -13,12 +13,12 @@ export default class ReactStoryDescription extends Component {
 
                 bordered={false}
                 title={story.title}>
-                    <Item label="Author">{ story.by }</Item>
+                    <Item>By: { story.by }</Item>
                     <Item label="Score">{ story.score }</Item>
                     <Item label="Replies">{ story.descendants }</Item>
-                    <Item label="Created">{ new Date(story.time).toUTCString() }</Item>
+                    <Item>Posted on: { new Date(story.time * 1000).toUTCString() }</Item>
                     <Item label="Type"><Tag color="green">{story.type}</Tag></Item>    
-                    <Item label="External Link"><Button type="primary" href={story.url} target="_blank">External Link</Button></Item> 
+                    <Item><Button href={story.url} target="_blank">Visit Source</Button></Item> 
                 </Descriptions>   
                 { story.kids ? <ReactComments kids={story.kids}/> : null}
             </div>
