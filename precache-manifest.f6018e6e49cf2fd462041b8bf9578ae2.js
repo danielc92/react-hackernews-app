ec416da933d18d6515f617fcd5f5f02b51aa706f
Daniel Corcoran <danielc.92@outlook.com>
2019-08-02 12:52:11 +1000

    Updates

---
+++ precache-manifest.f6018e6e49cf2fd462041b8bf9578ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f68abf8aece4ec3dfc674998f78676e",
+    "url": "/react-hackernews-app/index.html"
+  },
+  {
+    "revision": "f5f09b034e7c49afa8d6",
+    "url": "/react-hackernews-app/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "f5f09b034e7c49afa8d6",
+    "url": "/react-hackernews-app/static/js/2.270fc218.chunk.js"
+  },
+  {
+    "revision": "94e081db66da533582d7",
+    "url": "/react-hackernews-app/static/js/main.a42fa9d4.chunk.js"
+  },
+  {
+    "revision": "ea12d6a8f7d06235aba1",
+    "url": "/react-hackernews-app/static/js/runtime~main.565cd0b7.js"
+  },
+  {
+    "revision": "4627c20d3c15a9283a45ad728176af18",
+    "url": "/react-hackernews-app/static/media/snooping.4627c20d.svg"
+  }
+]);
